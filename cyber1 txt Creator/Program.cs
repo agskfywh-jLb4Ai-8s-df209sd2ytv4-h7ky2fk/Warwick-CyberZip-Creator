5462305d6def6c7e7ee6e0ae435b5c4bbdc6a41a
An Unsocial Pigeon <66815528+AnUnsocialPigeon@users.noreply.github.com>
2021-12-24 19:45:43 +0000

    Add project files.

---
+++ cyber1 txt Creator/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static System.Text.Encoding;
+
+namespace cyber1_txt_Creator {
+    class Program {
+        // Setup Vars
+        private static readonly string Cyber1Dir = Directory.GetCurrentDirectory() + @"\cyber1";
+        private static readonly string Cyber2Dir = Directory.GetCurrentDirectory() + @"\cyber2";
+        private static readonly string ReadMeDir = Directory.GetCurrentDirectory() + @"\README";
+        private static readonly string UndoneDir = Directory.GetCurrentDirectory() + @"\cyber2Undone";
+
+        private static readonly string Key = "WarwickApplication";
+
+        private static List<RowClass> Cyber1Data = new List<RowClass>() {
+                new RowClass("CyberFirst Advanced", "summer school", "advanced", new DateTime(2021, 8, 16), 10, "I participated in CyberFirst Advanced run by the National Cyber Security Centre. Here I earned the CyberFirst Advanced (SCQF Level 6) certificate."),
+                new RowClass("Collins Aerospace Work Experience", "work/placement project", "", new DateTime(2021, 8, 2), 5, "I did work experience at Collins Aerospace - a company which is a pioneer in aerospace technology. They taught me industry level cyber security and networking."),
+                new RowClass("Borwells Ltd Work Experience", "work/placement project", "", new DateTime(2021, 8, 9), 5, "I did work experience at Borwells Ltd. I learned industry standard programming (and practice) along with how both a software and cyber company operates day by day."),
+                new RowClass("Networking ALevel Projects", "own project", "", new DateTime(2021, 10, 26), 26, "I have created and coded my own networking protocols following a star network topology with socket programming for both my ALevel and personal projects."),
+                new RowClass("Cyber Centurion Competition", "other", "", new DateTime(2021, 10, 22), 6, "I have competed in the Cyber Centurion run by the American Air Force Association."),
+                new RowClass("Cyber Apprenticeship Development Scheme (CADS)", "course", "", new DateTime(2021, 9, 14), 12, "I am currently participating in CADS which is teaching everything from reconnaissance and OSINT to interview techniques and CV development."),
+                new RowClass("Frontend developer for the Malvern Cube Website", "work/placement project", "", new DateTime(2020, 1, 1), 50, "(Continuous) I am the front end web developer for the Malvern Cube. I also help with creating advertisements for their social media and employment adverts."),
+                new RowClass("Created created and presented a C# webinar", "own project", "", new DateTime(2020, 10, 12), 2, "I created and presented a C# basics webinar for my peers. It has successfully improved their ability at coding in C# and I received positive feedback for it."),
+                new RowClass("Presented created and presented a GitHub Basics presentation.", "own project", "", new DateTime(2021, 11, 8), 1, "I created and presented a 'How to use GitHub' presentation. It successfully taught my peers how to use and utilise GitHub for their ALevel projects."),
+                new RowClass("CyberFirst Futures", "summer school", "", new DateTime(2020, 8, 3), 10, "I participated in CyberFirst Futures run by the National Cyber Security Centre. Here I earned the CyberFirst Futures (SCQF Level 5) certificate"),
+                new RowClass("Cyber Discovery", "other", "", new DateTime(2019, 1, 17), 75, "I participated in CyberStart Assess along with all future rounds (Including being invited to complete the SANS foundation). I developed and practiced the fundamental Penetration Testing skills here."),
+                new RowClass("Ran a Coding Club", "other", "", new DateTime(2019, 1, 7), 25, "I co-hosted a Coding Club teaching Python for Years 7 to 9."),
+            };
+
+        private static string Cyber2Data = "I thaught that there would be no better way to creatively communicate how both CyberFirst Advanced and the Work Experience at Collins Aerospace than to utilise even just the most basics of encryption " +
+            "that I have learned. I love a challenge, and although the instructions of how to undo this were given in README, doing it all in the Linux Shell would pose a fun challenge. Other solutions to my 'puzzle' is to read this through " +
+            "GitHub on the burner account I created, purposefully set up to be near-unfindable without a direct link.\nWhen talking about each of the activities; CyberFirst Advanced was instrumental in teaching me the intermediate areas of Cyber Security " +
+            "and Collins Aerospace taught me alot from practical and quick programming, to industry standard Networking and infrastructure. I was able to help configure switches, a PXE server, and firewalls. Using Ansible to automate configuration, and " +
+            "python to aid in edge detection was just a few of the activities I participated in.";
+
+
+        private static string ReadMeData = $"To undo my encryption, XOR cyber2 with the key {Key}, and then flip every bit. " +
+            "Should you be unable to in the Linux Shell, see https://github.com/agskfywh-jLb4Ai-8s-df209sd2ytv4-h7ky2fk";
+       
+
+        public static void Main() {
+            Cyber1Handler();
+
+            for (int i = Cyber2Data.Length; i < 1000; i++) {
+                Cyber2Data += " ";
+            }
+
+            byte[] cyber2;
+            cyber2 = Cyber2Handler_BitFlip(ASCII.GetBytes(Cyber2Data));
+            cyber2 = Cyber2Handler_XORKey(cyber2, ASCII.GetBytes(Key));
+            File.WriteAllBytes(Cyber2Dir, cyber2);
+
+            cyber2 = File.ReadAllBytes(Cyber2Dir);
+            cyber2 = Cyber2Handler_XORKey(cyber2, ASCII.GetBytes(Key));
+            cyber2 = Cyber2Handler_BitFlip(cyber2);
+            File.WriteAllBytes(UndoneDir, cyber2);
+
+
+
+            // ReadMe stuff
+            Console.WriteLine($"ReadMe bytes remaining: {200 - ReadMeData.Length}");
+            for (int i = ReadMeData.Length; i < 200; i++) {
+                ReadMeData += " ";
+            }
+            File.WriteAllBytes(ReadMeDir, ASCII.GetBytes(ReadMeData));
+
+            Console.ReadLine();
+        }
+
+        private static void Cyber1Handler() {
+            // File Clears
+            File.WriteAllText(Cyber1Dir, "");
+
+            // File writing
+            foreach (RowClass activity in Cyber1Data) {
+                File.WriteAllBytes(Cyber1Dir, Encoding.ASCII.GetBytes((
+                    File.ReadAllText(Cyber1Dir) + "\n" + activity.DataAsCSV()).Trim()));
+            }
+
+            // Byte Whitespacing
+            byte[] f = File.ReadAllBytes(Cyber1Dir);
+            string toAppend = "\nWhitespace Line,other,,0000-00-00,0,";
+            string dataExpansion = "Whitespace ";
+            int initialLength = f.Length + toAppend.Length;
+
+            if (initialLength >= 2500) {
+                Console.WriteLine($"Over byte limit by {f.Length + toAppend.Length - 2500}");
+                Console.ReadLine();
+                return;
+            }
+
+            for (int i = initialLength; i < 2500; i++) {
+                toAppend += dataExpansion[i % dataExpansion.Length];
+            }
+
+            File.WriteAllBytes(Cyber1Dir, ASCII.GetBytes((File.ReadAllText(Cyber1Dir) + toAppend).Trim()));
+
+            Console.WriteLine($"Cyber1 Success! {2500 - initialLength} bytes remaining.");
+        }
+    
+        private static byte[] Cyber2Handler_BitFlip(byte[] input) {
+            for (int i = 0; i < input.Length; i++) {
+                input[i] ^= 255;
+            }
+            Console.WriteLine($"Cyber2 Success! {1000 - input.Length} remaining.");
+            return input;
+
+        }
+    
+        private static byte[] Cyber2Handler_XORKey(byte[] input, byte[] key) {
+            for (int i = 0; i < input.Length; i++) {
+                input[i] ^= key[i % key.Length];
+            }
+            Console.WriteLine($"Cyber2 Success! {1000 - input.Length} remaining.");
+            return input;
+        }
+    
+    }
+}
---
+++ cyber1 txt Creator/RowClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cyber1_txt_Creator {
+    public class RowClass {
+        private string _type;
+        private string _description;
+
+        public string Name { get; set; }
+        public string Type { get => _type; set { _type = TypeValidator(value.ToLower()) ? value.ToLower() : throw new Exception("Invalid string Input"); } }
+        public string Level { get; set; }
+        public DateTime StartDate { get; set; }
+        public int WorkingDays { get; set; }
+        public string Description { get => _description; set { _description = value.Length <= 200 && !value.Contains(',') ? value : throw new Exception("Input too long"); } }
+
+        /// <summary>
+        /// Creates a new instance of the Row Class.
+        /// </summary>
+        public RowClass() { }
+
+        /// <summary>
+        /// Creates a new instance of the Row Class.
+        /// </summary>
+        /// <param name="name">The name of the course</param>
+        /// <param name="type">The type of course (summer school, course, school/college project, work/placement project, own project, other)</param>
+        /// <param name="level">Introduction, advanced, elite, {blank}</param>
+        /// <param name="startDate">Format YYYY-MM-DD</param>
+        /// <param name="workingDays">Duration in working days</param>
+        /// <param name="description">Description</param>
+        public RowClass(string name, string type, string level, DateTime startDate, int workingDays, string description) {
+            Name = name;
+            Type = type;
+            Level = level;
+            StartDate = startDate;
+            WorkingDays = workingDays;
+            Description = description;
+        }
+
+        private bool TypeValidator(string type) {
+            string[] compareTo = new string[] { "summer school", "course", "school/college project", "work/placement project", "own project", "other" };
+            return compareTo.Contains(type);
+        }
+
+
+        public string DataAsCSV() {
+            return $"{Name},{Type},{Level},{StartDate.ToString("yyyy-MM-dd")},{WorkingDays.ToString()},{Description}";
+        }
+    }
+}
